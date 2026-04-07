fbe556fecd804c97a831d0ac3cd176cb9b999cf1
mahmoud <mahmoudshalaby363@gmail.com>
2026-04-07 13:36:23 +0200

    feat(movies): reuse component that show movies adding top-rating,upcoming,popular

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import HeroSection from "@/Components/Hero/HeroSection";
 import Movies from './../Components/Movies/Movies';
+import PopularMovies from './../Components/Movies/Popular/PopularMovies';
+import TopRatingMovies from './../Components/Movies/TopRating/TopRating';
+import UpcommingMovies from './../Components/Movies/Upcoming/UpcomingMovies';
 
 export default function Home() {
   return (
@@ -9,6 +12,9 @@ export default function Home() {
       </div>
       <div className="container mx-auto">
         <Movies />
+        <PopularMovies />
+        <TopRatingMovies />
+        <UpcommingMovies />
       </div>
     </>
   );
